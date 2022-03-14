a9d0637ff7420ca58e26e97ab13d735bc7e4107d
SupinePandora43 <supinepandoraggg@gmail.com>
2022-03-15 00:16:18 +0500

    Implement LinuxNativeLibrary (#1)
    * Implement LinuxNativeLibrary
    
    * Create Libdl.cs
    
    * Fix missing dlopen parameter

---
+++ RenderDocGen/Evergine.Bindings.RenderDoc/Libdl.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Evergine.Bindings.RenderDoc
+{
+    internal static class Libdl
+    {
+        [DllImport("libdl")]
+        public static extern IntPtr dlopen(string fileName, int flags);
+
+        [DllImport("libdl")]
+        public static extern IntPtr dlsym(IntPtr module, string procName);
+
+        [DllImport("libdl")]
+        public static extern void dlclose(IntPtr module);
+    }
+}
